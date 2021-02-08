5f1e36429a7cc5da37c37274b93e8e7d7f8fef95
r2williams <rebecca.williams@historicengland.org.uk>
2021-02-08 10:08:37 +0000

    multiple pointers

--- multiplePointers.js
+++ multiplePointers.js
@@ -44,3 +44,47 @@ function areThereDuplicates(){
         return false;
     }
 }
+
+// Challenge
+
+/* Wite a function called averagePair. Given a sorted
+array of integers and a target average, determine if there
+is a pair of values in the array where the average of the
+pair equals the target average. There may be more than one
+pair that matches the average target. */
+
+function averagePair(arr, value){
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let average = (arr[left] + arr[right]) / 2
+        if (average === value) return true;
+        else if (average < value) left++;
+        else right--;
+        }
+        return false;
+    }
+    averagePair([1,2,2,3,4,4,5,6,7], 2)
+
+// Challenge
+
+/* Write a function called isSubsequence which takes in
+two strings and checks whether the characters in the first
+string form a subsequence of the characters in the second
+string. In other words, the function should check whether
+the characters in the first string appear somewhere in the 
+second string, without their order changing. */
+
+function isSubsequence(str1, str2){
+    var i = 0;
+    var j = 0;
+
+    if (!str1) return true;
+    while (j < str2.length) {
+        if (str2[j] === str1[i]) i++;
+        if (i === str1.length) return true;
+        j++;
+    }
+    return false;
+}
