b21474583752ebabce4b055c98dd3e9439013836
r2williams <rebecca.williams@historicengland.org.uk>
2021-02-08 10:07:52 +0000

    Sliding window and multiple pointers

--- slidingWindow.js
+++ slidingWindow.js
@@ -24,3 +24,28 @@ function maxSubarraySum(arr, num){ // no relooping
     return maxSum;
 }
 maxSubarraySum([2,6,9,2,1,8,5,6,3], 3)
+
+// Challenge - minSubArrayLen
+
+/* Write a function called minSubArrayLen which 
+accepts two parameters - an array of positive
+integers and a positive integer. The function
+should return the minimal length of a contiguous
+subarray of which the sum is greater than or equal
+to the integer passed to the function. If there isn't
+one, return 0 instead. */
+
+function minSubArrayLen(arr, num) {
+    let sum = 0;
+    let temSum = 0;
+
+    if (arr.length < num) return 0;
+    for (let i = 0; i < num; i++) {
+        sum += arr[i];
+    }
+
+    tempSum = sum;
+    
+
+
+}
